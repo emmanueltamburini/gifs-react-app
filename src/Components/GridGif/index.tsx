7284332303777243b41b62ed33cd42a8bca37f45
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-07-18 06:42:38 -0400

    loading has added

--- src/Components/GridGif/index.tsx
+++ src/Components/GridGif/index.tsx
@@ -11,6 +11,7 @@ export const GridGif = ({ category }: Props) => {
     return (
         <>
             <h3>{category}</h3>
+            {isLoading && <h2>Loading...</h2>}
             <div className="card-grid">
                 {images.map(image => {
                     return (
